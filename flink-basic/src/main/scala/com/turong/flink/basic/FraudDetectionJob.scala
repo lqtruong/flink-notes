542371457ea4c29be86a1d04a76f15f2593eb1a0
turong <lqtruong@gmail.com>
2022-08-13 21:51:16 +0700

    Add Basic Fraud Detection Job

---
+++ flink-basic/src/main/scala/com/turong/flink/basic/FraudDetectionJob.scala
@@ -0,0 +1,13 @@
+package com.turong.flink.basic
+
+import org.apache.flink.streaming.api.scala._
+
+object FraudDetectionJob {
+
+  def main(args: Array[String]) {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    env.execute("Flink Fraud Detection Job")
+  }
+}
